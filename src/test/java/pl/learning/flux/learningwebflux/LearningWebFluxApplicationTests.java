b864591d7e4624813cd2b8b5c5f29aecede3fedd
Marcin Mikowski <m.mikowski@wp.pl>
2024-02-16 15:25:33 +0100

    Creating webflux application

---
+++ src/test/java/pl/learning/flux/learningwebflux/LearningWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.learning.flux.learningwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
